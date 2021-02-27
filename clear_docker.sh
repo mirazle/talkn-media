017e5164036fe87a9c560189b9b31a54e037cd64
mirazle <mirazle2069@gmail.com>
2021-02-27 16:45:59 +0900

    docker

---
+++ clear_docker.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+docker ps -a | grep $1 | awk '{print $1}' | xargs docker rm
+docker rmi $1
---
+++ markdowns/docker.md
@@ -0,0 +1,2 @@
+docker run -d -p 3000:3000 7d38453b393c
+docker run -dit 7d38453b393c -p 3000:3000
